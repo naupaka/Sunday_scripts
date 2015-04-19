90024c0d252ac510f9039a94a2d84ebe52fc6858
Naupaka Zimmerman <naupaka@gmail.com>
2015-04-19 14:29:57 -0500

    Add challenge

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -4,7 +4,7 @@
 # April 19th, 2015
 
 # Install dplyr package and dependencies
-install.packages("dplyr", dependencies = TRUE)
+# install.packages("dplyr", dependencies = TRUE)
 
 # Load dplyr library into working environment
 library("dplyr")
@@ -14,7 +14,12 @@ gap.in <- read.table("output/combined_gapMinder.tsv",
                      sep = "\t", 
                      header = TRUE) 
 
+gap.in %>%
+    filter(country == "China") %>%
+    select(country, year, pop) %>%
+    group_by(country) %>%
+    summarize(min = min(pop))
 
-
-
+# Challenge
+# Calculate the mean population per continent per year for years prior to 1990
 
